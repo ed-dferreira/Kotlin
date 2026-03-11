74eec14cb99f7fd85318aed27cb1d67b5c8fba85
Eduardo Ferreira <eduardoodferreiradev@gmail.com>
2026-03-11 20:14:50 -0300

    Criação do CurrencyField.kt

--- ConversorDeMoedas/app/src/main/java/com/ed_dferreira/conversordemoedas/ui/components/CurrencyField.kt
+++ ConversorDeMoedas/app/src/main/java/com/ed_dferreira/conversordemoedas/ui/components/CurrencyField.kt
@@ -2,14 +2,20 @@ package com.ed_dferreira.conversordemoedas.ui.components
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.ed_dferreira.conversordemoedas.ui.theme.ConversorDeMoedasTheme
 
@@ -38,7 +44,33 @@ fun CurrencyField(
             )
             OutlinedTextField(
                 value = currencyAmount,
-                onCurrencyAmountChanged
+                onCurrencyAmountChanged,
+                modifier = Modifier
+                    .weight(1f), //empurra o componente
+                textStyle = MaterialTheme.typography.titleLarge.copy(
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.End,
+                ),
+                placeholder = {
+                    Text(
+                        text = "0", // ver pq não tá aparecendo o 0
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        style = MaterialTheme.typography.titleLarge.copy(
+                            fontWeight = FontWeight.Bold,
+                            textAlign = TextAlign.End,
+                        )
+
+                    )
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Decimal,
+                ),
+                colors = OutlinedTextFieldDefaults.colors( //sobreescreve as cores do matherial no componente
+                    focusedBorderColor = Color.Transparent,
+                    unfocusedBorderColor = Color.Transparent,
+                    errorBorderColor = Color.Transparent,
+                )
             )
         }
     }
@@ -51,7 +83,7 @@ private fun CurrencyFieldPreview() {
         CurrencyField(
             currencies = listOf("USD", "EUR", "BRL"),
             selectedCurrency = "USD",
-            "100",
+            "",
             {},
             onCurrencyAmountChanged = {}
         )
